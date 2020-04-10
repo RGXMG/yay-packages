a65854ad34780fa9422d62b035b31d40573b8980
xmg <759237309@qq.com>
2020-04-10 12:13:51 +0800

    重新treeData方法

--- packages/yay-util/packages/collection/treeData.ts
+++ packages/yay-util/packages/collection/treeData.ts
@@ -1,5 +1,5 @@
 import isFunction from '../function/isFunction';
-import isArrary from '../array/isArray';
+import isArray from '../array/isArray';
 
 interface IOpt<T> {
   // 自身的标识的属性名称，默认为id
@@ -10,8 +10,6 @@ interface IOpt<T> {
   childrenKey?: string;
   // 顶层父对象自身的标识的值，默认为0
   topVal?: number | string | boolean | null | undefined;
-  // 提供一个方法，进行筛选整个data
-  filterCb?: (args: Array<T>) => Array<T>;
   // 当遍历每一个对象时，该方法可以删除或者增加属性
   cutCb?: (args: T) => Partial<T>;
 }
@@ -23,61 +21,37 @@ interface IOpt<T> {
  * @returns {*}
  */
 function index<T>(data: Array<T>, opt?: IOpt<T>): Object[] {
-  if (!isArrary(data)) throw new Error('treeData: data必须为Array<Object>！');
+  if (!isArray(data)) throw new Error('treeData: data必须为Array<Object>！');
   const defaultOpts = {
     key: 'id',
     parentKey: 'pid',
     childrenKey: 'children',
     topVal: 0,
-    filterCb: null,
     cutCb: null,
   };
   const opts = { ...defaultOpts, ...opt };
-  const childArr = [];
-  const parentArr = [];
-  // 分开顶级节点以及其所有子节点
-  (isFunction(opts.filterCb) ? opts.filterCb(data) : data).forEach(function(item) {
-    let newItem = item;
-    if (isFunction(opts.cutCb)) newItem = opts.cutCb(item);
-    if (newItem[opts.parentKey] !== opts.topVal) {
-      childArr.push(newItem);
-    } else parentArr.push(newItem);
-  });
-  /**
-   * 找到当前对象的最底层的children
-   * @param obj
-   * @param cb
-   */
-  const findLastChild = function findLastChild(obj, cb) {
-    if (obj[opts.childrenKey]) {
-      obj[opts.childrenKey].forEach(function(item) {
-        return findLastChild(item, cb);
-      });
+  // item映射
+  const itemKvMap = data.reduce((map, cur) => {
+    let nCur = cur;
+    if (isFunction(opts.cutCb)) {
+      nCur = opts.cutCb(nCur);
     }
-    cb(obj);
-  };
-  /**
-   * 为顶层父元素插入子元素
-   * @param init
-   * @returns {*}
-   */
-  const insertChild = function insertChild(init) {
-    return childArr.reduce(function(total, cur, i) {
-      let isInsert = false;
-      findLastChild(total, function(obj) {
-        if (obj[opts.key] === cur[opts.parentKey]) {
-          obj[opts.childrenKey] = obj[opts.childrenKey]
-            ? [].concat(obj[opts.childrenKey], [cur])
-            : [cur];
-          isInsert = true;
+    return (map[cur[opts.key]] = nCur), map;
+  }, {});
+  // 找寻父元素
+  data.forEach(item => {
+    if (`${item[opts.parentKey]}` !== `${opts.topVal}`) {
+      const parentKey = item[opts.parentKey];
+      const parentItem = itemKvMap[parentKey];
+      if (parentItem) {
+        if (!isArray(parentItem[opts.childrenKey])) {
+          parentItem[opts.childrenKey] = [];
         }
-      });
-      if (i === data.length && isInsert) insertChild(total);
-      return total;
-    }, init);
-  };
-  return parentArr.map(function(parent) {
-    return insertChild(parent);
+        parentItem[opts.childrenKey].push(item);
+      }
+    }
   });
+  // filter 顶级元素
+  return data.filter(item => `${item[opts.parentKey]}` === `${opts.topVal}`);
 }
 export default index;
